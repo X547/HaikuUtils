19d6f1e603b3ca0a5ad030d6492ecf4cee98653a
Humdinger <humdinger@mailbox.org>
2023-12-31 17:13:36 +0100

    Implement ArgvReceived()
    To open the file given as argument from the command line.

--- MsgDump/MsgDump.cpp
+++ MsgDump/MsgDump.cpp
@@ -580,6 +580,25 @@ TestApplication::TestApplication(): BApplication("application/x-vnd.Test-MsgDump
 {
 }
 
+
+void TestApplication::ArgvReceived(int32 argc, char** argv)
+{
+	if (argc <= 1)
+		return;
+
+	BMessage refsReceived(B_REFS_RECEIVED);
+	for (int32 i = 1; i < argc; i++) {
+		BEntry entry(argv[i], true);
+		entry_ref ref;
+		if (entry.GetRef(&ref) == B_OK)
+			refsReceived.AddRef("refs", &ref);
+	}
+
+	if (!refsReceived.IsEmpty())
+		RefsReceived(&refsReceived);
+}
+
+
 void TestApplication::RefsReceived(BMessage *refsMsg)
 {
 	entry_ref ref;
